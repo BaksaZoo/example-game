58a1817b2c88356abc495d4d6e67d90966a1426a
Baksa Zoltán <baksa.zoltan@otpmobil.com>
2025-04-17 18:27:03 +0200

    feat(main): add 'q' key event to stop the game

--- src/main/java/hu/baksaz/example/game/Main.java
+++ src/main/java/hu/baksaz/example/game/Main.java
@@ -1,10 +1,19 @@
 package hu.baksaz.example.game;
 
+import hu.baksaz.example.engine.ApplicationContext;
 import hu.baksaz.example.engine.GameManager;
+import hu.baksaz.example.engine.InputEvent;
 
 public class Main {
   public static void main(String[] args) {
     GameManager gameManager = new GameManager();
+
+    ApplicationContext.getPublisher().subscribe(InputEvent.class, event -> {
+      if (event.getKey() == 'q') {
+        gameManager.stopGame();
+      }
+    });
+
     gameManager.startGame();
   }
 }
