5256182178969296a17215208c483a5e7b6f259c
Baksa Zoltán <baksa.zoltan@otpmobil.com>
2025-04-19 12:03:27 +0200

    refactor: rename 'player' to 'playerSprite' for clarity`

--- src/main/java/hu/baksaz/example/game/Player.java
+++ src/main/java/hu/baksaz/example/game/Player.java
@@ -7,34 +7,34 @@ import java.awt.event.KeyEvent;
 import java.util.StringJoiner;
 
 public class Player {
-  private Box2D player;
+  private Box2D playerSprite;
 
   public Player() {
-    this.player = new Box2D()
+    this.playerSprite = new Box2D()
       .setWidth(10)
       .setHeight(10)
       .setX(100)
       .setY(100)
       .setColor(Color.RED);
-    this.player.onClick(e -> player.setColor(Color.GREEN));
+    this.playerSprite.onClick(e -> playerSprite.setColor(Color.GREEN));
 
     ApplicationContext.getPublisher().subscribe(KeyEvent.class, this::handleInput);
   }
 
   private void handleInput(KeyEvent keyEvent) {
     switch (Character.toLowerCase(keyEvent.getKeyChar())) {
-      case 'w' -> player.setY(player.getY() - 10);
-      case 's' -> player.setY(player.getY() + 10);
-      case 'a' -> player.setX(player.getX() - 10);
-      case 'd' -> player.setX(player.getX() + 10);
+      case 'w' -> playerSprite.setY(playerSprite.getY() - 10);
+      case 's' -> playerSprite.setY(playerSprite.getY() + 10);
+      case 'a' -> playerSprite.setX(playerSprite.getX() - 10);
+      case 'd' -> playerSprite.setX(playerSprite.getX() + 10);
     }
-    System.out.println(player);
+    System.out.println(playerSprite);
   }
 
   @Override
   public String toString() {
     return new StringJoiner(", ", Player.class.getSimpleName() + "[", "]")
-      .add("player=" + player)
+      .add("player=" + playerSprite)
       .toString();
   }
 }
